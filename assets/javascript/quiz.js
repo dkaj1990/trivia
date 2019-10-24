64a4bbdb0e0db23973764423eb9c0c3f2dbac112
Dipti Katkar <dk.aj1990@gmail.com>
2019-10-23 23:57:54 -0500

    Added logic to compare answer

---
+++ assets/javascript/quiz.js
@@ -0,0 +1,15 @@
+const startBtn = document.getElementById('start-btn');
+ startBtn.addEventListener('click', startQuiz);
+
+
+function startQuiz(){
+    console.log('Started');
+ 
+}
+function setNextQuestion(){
+    
+}
+
+function selectAnswer(){
+
+}
